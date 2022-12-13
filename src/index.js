61419c8787b1b97330dc5799e34e4323f6d1a71e
mpreter <dshabelyan@mail.ru>
2022-12-13 18:30:27 +0300

    Change config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const authRouter = require("./authRouter.js");
 const PORT = process.env.PORT || 5000;
+const connectBD = process.env.MONGO_DB_CONNECTION || '';
 
 const app = express();
 
@@ -10,9 +11,7 @@ app.use("/auth", authRouter);
 
 const start = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://user:user1234@cluster0.qgytpnx.mongodb.net/?retryWrites=true&w=majority"
-    );
+    await mongoose.connect("mongodb+srv://user:user1234@cluster0.qgytpnx.mongodb.net/?retryWrites=true&w=majority");
     app.listen(PORT, () => console.log(`CONNECT ${PORT}`));
   } catch (e) {
     console.log(e);
